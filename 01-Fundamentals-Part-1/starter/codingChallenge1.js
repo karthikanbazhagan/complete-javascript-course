10d87fc2fb0cf327b2bcaafd011cf26ceb463673
karthikanbazhagan <karthiksanjay@live.com>
2020-12-14 15:24:18 +0530

    Section2: CodeChallenge1

---
+++ 01-Fundamentals-Part-1/starter/codingChallenge1.js
@@ -0,0 +1,28 @@
+// Coding Challenge: #1
+
+// Data Set: #1
+let weightOfMark = 78;
+let heightOfMark = 1.69;
+
+let weightOfJohn = 92;
+let heightOfJohn = 1.95;
+
+let bmiOfMark = weightOfMark / heightOfMark ** 2;
+let bmiOfJohn = weightOfJohn / heightOfJohn ** 2;
+
+let markHigherBmi = bmiOfMark > bmiOfJohn;
+console.log("1. Is Mark having higher BMI than John? " + markHigherBmi);
+
+
+// Data Set: #2
+weightOfMark = 95;
+heightOfMark = 1.88;
+
+weightOfJohn = 85;
+heightOfJohn = 1.76;
+
+bmiOfMark = weightOfMark / heightOfMark ** 2;
+bmiOfJohn = weightOfJohn / heightOfJohn ** 2;
+
+markHigherBmi = bmiOfMark > bmiOfJohn;
+console.log("2. Is Mark having higher BMI than John? " + markHigherBmi);
